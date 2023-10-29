b7ba308fd737c2a9836e6fbe8a31bd57a0a45f02
Peter <pn7000@gmail.com>
2023-10-29 14:06:58 -0500

    initial commit

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+    // Temporary code for simple end to end api call
+    //const [showApiMessage, setShowApiMessage] = useState(false)
+    const [apiMessage, setApiMessage] = useState('')
+    const handleGetApi = () => {
+        //setApiMessage()
+    }
+
+    return (
+        <div>Home
+            <button>Click me</button>
+            {apiMessage && <p>{apiMessage}</p>}
+        </div>
+    )
+}
+
+export default Home
